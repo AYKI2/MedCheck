dc493924adc1333308e45840d663be0b3d1b2546
AYKI2 <abdulhamitov.bstn@gmail.com>
2023-04-18 17:10:01 +0600

    clean code

--- src/main/java/com/example/medcheckb8/db/entities/Account.java
+++ src/main/java/com/example/medcheckb8/db/entities/Account.java
@@ -6,10 +6,8 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
-
 import static jakarta.persistence.CascadeType.ALL;
 @Getter
 @Setter
