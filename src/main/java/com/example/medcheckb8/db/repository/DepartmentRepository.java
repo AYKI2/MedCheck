a8e99090d42bcaff37df4391eceaf1b5bc270788
Kuban <kubanmuit@gmail.com>
2023-04-20 13:16:09 +0600

    pull in main page

--- src/main/java/com/example/medcheckb8/db/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medcheckb8.db.repository;
-
-import com.example.medcheckb8.db.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
+++ src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
@@ -2,11 +2,9 @@ package com.example.medcheckb8.db.service.impl;
 
 import com.example.medcheckb8.db.dto.request.ResultRequest;
 import com.example.medcheckb8.db.dto.response.SimpleResponse;
-import com.example.medcheckb8.db.entities.Department;
 import com.example.medcheckb8.db.entities.Result;
 import com.example.medcheckb8.db.entities.User;
 import com.example.medcheckb8.db.exceptions.NotFountException;
-import com.example.medcheckb8.db.repository.DepartmentRepository;
 import com.example.medcheckb8.db.repository.ResultRepository;
 import com.example.medcheckb8.db.repository.UserRepository;
 import com.example.medcheckb8.db.service.ResultService;
@@ -19,7 +17,7 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class ResultServiceImpl implements ResultService {
     private final ResultRepository resultRepository;
-    private final DepartmentRepository departmentRepository;
+//    private final DepartmentRepository departmentRepository;
     private final UserRepository userRepository;
     @Override
     public SimpleResponse addResult(Long patientId, ResultRequest request) {
@@ -27,12 +25,12 @@ public class ResultServiceImpl implements ResultService {
                 .orElseThrow(() -> new NotFountException(
                         String.format("Patient with id: %d doesn't exist.", patientId)));
 
-        Department department = departmentRepository.findById(request.departmentId())
-                .orElseThrow(() -> new NotFountException(
-                        String.format("Department with id: %d doesn't exist.", request.departmentId())));
+//        Department department = departmentRepository.findById(request.departmentId())
+//                .orElseThrow(() -> new NotFountException(
+//                        String.format("Department with id: %d doesn't exist.", request.departmentId())));
 
         Result result = Result.builder()
-                .department(department)
+//                .department(department)
                 .dateOfIssue(request.dateOfIssue())
                 .orderNumber(generateOrderNumber())
                 .file(request.file())
