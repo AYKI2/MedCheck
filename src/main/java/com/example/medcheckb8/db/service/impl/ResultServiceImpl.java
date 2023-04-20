04dbc99ec8b179d0053d7b08ad3d22105df6c872
Kuban <kubanmuit@gmail.com>
2023-04-20 22:13:43 +0600

    checkout main page

--- src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
+++ src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
@@ -2,13 +2,16 @@ package com.example.medcheckb8.db.service.impl;
 
 import com.example.medcheckb8.db.dto.request.ResultRequest;
 import com.example.medcheckb8.db.dto.response.SimpleResponse;
+import com.example.medcheckb8.db.entities.Department;
 import com.example.medcheckb8.db.entities.Result;
 import com.example.medcheckb8.db.entities.User;
 import com.example.medcheckb8.db.exceptions.NotFountException;
+import com.example.medcheckb8.db.repository.DepartmentRepository;
 import com.example.medcheckb8.db.repository.ResultRepository;
 import com.example.medcheckb8.db.repository.UserRepository;
 import com.example.medcheckb8.db.service.ResultService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -17,35 +20,38 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class ResultServiceImpl implements ResultService {
     private final ResultRepository resultRepository;
-//    private final DepartmentRepository departmentRepository;
+    private final DepartmentRepository departmentRepository;
     private final UserRepository userRepository;
+
     @Override
     public SimpleResponse addResult(Long patientId, ResultRequest request) {
-        User user = userRepository.findById(request.departmentId())
+        User user = userRepository.findById(patientId)
                 .orElseThrow(() -> new NotFountException(
                         String.format("Patient with id: %d doesn't exist.", patientId)));
 
-//        Department department = departmentRepository.findById(request.departmentId())
-//                .orElseThrow(() -> new NotFountException(
-//                        String.format("Department with id: %d doesn't exist.", request.departmentId())));
+        Department department = departmentRepository.findById(request.departmentId())
+                .orElseThrow(() -> new NotFountException(
+                        String.format("Department with id: %d doesn't exist.", request.departmentId())));
 
         Result result = Result.builder()
-//                .department(department)
+                .department(department)
                 .dateOfIssue(request.dateOfIssue())
                 .orderNumber(generateOrderNumber())
                 .file(request.file())
+                .user(user)
                 .build();
 
         user.addResult(result);
         resultRepository.save(result);
 
         return SimpleResponse.builder()
+                .status(HttpStatus.OK)
                 .message(String.format("Result with patient full name: %s successfully added.",
                         (user.getFirstName() + " " + user.getLastName())))
                 .build();
     }
 
-    private String generateOrderNumber(){
+    private String generateOrderNumber() {
         Random random = new Random();
         int length = 18;
         String chars = "0123456789abcdefghijklmnopqrstuvwxyz";
