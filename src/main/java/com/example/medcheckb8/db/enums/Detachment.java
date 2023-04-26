ea4828b488e73dd3b749ea1b73286ed5f98ef0b3
uluk279 <hoksber@gmail.com>
2023-04-26 12:35:58 +0300

    add search in Doctor

--- src/main/java/com/example/medcheckb8/db/enums/Detachment.java
+++ src/main/java/com/example/medcheckb8/db/enums/Detachment.java
@@ -1,32 +1,27 @@
 package com.example.medcheckb8.db.enums;
 
 
-
-
 public enum Detachment {
-    ALLERGOLOGY("Аллергология"),
-    ANESTHESIOLOGY("Анестезиология"),
-    VACCINATION("Вакцинация"),
-    GASTROENTEROLOGY("Гастроэнтерология"),
-    GYNECOLOGY("Гинекология"),
-    DERMATOLOGY("Дерматология"),
-    CARDIOLOGY("Кардиология"),
-    NEUROLOGY("Неврология"),
-    NEUROSURGERY("Нейрохирургия"),
-    ONCOLOGY("Онкология"),
-    ORTHOPEDICS("Ортопедия"),
-    OTORHINOLARYNGOLOGY("Оториноларингология"),
-    OPHTHALMOLOGY("Офтальмология"),
-    PROCTOLOGY("Проктология"),
-    PSYCHOTHERAPY("Психтерапия"),
-    PULMONOLOGY("Пульмонология"),
-    RHEUMATOLOGY("Ревмотология"),
-    THERAPY("Терапия"),
-    UROLOGY("Урология"),
-    PHLEBOLOGY("Флебология"),
-    PHYSIOTHERAPY("Физиотерапия"),
-    ENDOCRINOLOGY("Эндокринология");
-
-    Detachment(String translate) {
-    }
+    ALLERGOLOGY,
+    ANESTHESIOLOGY,
+    VACCINATION,
+    GASTROENTEROLOGY,
+    GYNECOLOGY,
+    DERMATOLOGY,
+    CARDIOLOGY,
+    NEUROLOGY,
+    NEUROSURGERY,
+    ONCOLOGY,
+    ORTHOPEDICS,
+    OTORHINOLARYNGOLOGY,
+    OPHTHALMOLOGY,
+    PROCTOLOGY,
+    PSYCHOTHERAPY,
+    PULMONOLOGY,
+    RHEUMATOLOGY,
+    THERAPY,
+    UROLOGY,
+    PHLEBOLOGY,
+    PHYSIOTHERAPY,
+    ENDOCRINOLOGY
 }
