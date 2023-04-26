b1f1498447163ac6d64612a3721e6091e68f579b
Eliza <elizashyrbaeva@gmail.com>
2023-04-26 15:48:13 +0600

    create repository

---
+++ src/main/java/com/example/medcheckb8/db/dto/response/ScheduleResponse.java
@@ -0,0 +1,18 @@
+package com.example.medcheckb8.db.dto.response;
+
+import com.example.medcheckb8.db.enums.Repeat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+
+@Builder
+public record ScheduleResponse(Long id,
+                               Repeat repeatDay,
+                               Map<LocalTime, LocalTime> times,
+                               LocalDate date,
+                               String position,
+                               String fullName,
+                               String image) {
+}
