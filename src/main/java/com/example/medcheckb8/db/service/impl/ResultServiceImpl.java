8a173eb4703b09b6ddca7e282b6f2f1f83038932
Eliza <elizashyrbaeva@gmail.com>
2023-05-08 09:07:23 +0600

    logging result class

--- src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
+++ src/main/java/com/example/medcheckb8/db/service/impl/ResultServiceImpl.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
@@ -23,33 +25,42 @@ public class ResultServiceImpl implements ResultService {
     private final ResultRepository resultRepository;
     private final DepartmentRepository departmentRepository;
     private final UserRepository userRepository;
+    private static final Logger logger = Logger.getLogger(Result.class.getName());
 
     @Override
     public SimpleResponse addResult(ResultRequest request) {
-        User user = userRepository.findById(request.patientId())
-                .orElseThrow(() -> new NotFountException(
-                        String.format("Patient with id: %d doesn't exist.", request.patientId())));
+        try {
+            User user = userRepository.findById(request.patientId())
+                    .orElseThrow(() -> new NotFountException(
+                            String.format("Patient with id: %d doesn't exist.", request.patientId())));
 
-        Department department = departmentRepository.findById(request.departmentId())
-                .orElseThrow(() -> new NotFountException(
-                        String.format("Department with id: %d doesn't exist.", request.departmentId())));
+            Department department = departmentRepository.findById(request.departmentId())
+                    .orElseThrow(() -> new NotFountException(
+                            String.format("Department with id: %d doesn't exist.", request.departmentId())));
 
-        Result result = Result.builder()
-                .department(department)
-                .dateOfIssue(request.dateOfIssue())
-                .orderNumber(uniquenessCheckOrderNumber())
-                .file(request.file())
-                .user(user)
-                .build();
+            Result result = Result.builder()
+                    .department(department)
+                    .dateOfIssue(request.dateOfIssue())
+                    .orderNumber(uniquenessCheckOrderNumber())
+                    .file(request.file())
+                    .user(user)
+                    .build();
 
-        user.addResult(result);
-        resultRepository.save(result);
+            user.addResult(result);
+            resultRepository.save(result);
+            logger.log(Level.INFO, String.format("Result with patient full name: %s successfully added.",
+                    (user.getFirstName() + " " + user.getLastName())));
 
-        return SimpleResponse.builder()
-                .status(HttpStatus.OK)
-                .message(String.format("Result with patient full name: %s successfully added.",
-                        (user.getFirstName() + " " + user.getLastName())))
-                .build();
+            return SimpleResponse.builder()
+                    .status(HttpStatus.OK)
+                    .message(String.format("Result with patient full name: %s successfully added.",
+                            (user.getFirstName() + " " + user.getLastName())))
+                    .build();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed to add result", e);
+            throw e;
+        }
+
     }
 
     private String generateOrderNumber() {
@@ -62,17 +73,21 @@ public class ResultServiceImpl implements ResultService {
             int index = random.nextInt(chars.length());
             sb.append(chars.charAt(index));
         }
-        return sb.toString();
+        String orderNumber = sb.toString();
+        logger.info(String.format("Generated order number: %s", orderNumber));
+        return orderNumber;
     }
 
     private String uniquenessCheckOrderNumber() {
         List<Result> all = resultRepository.findAll();
         String s = generateOrderNumber();
         for (Result result : all) {
-            if (s.equals(result.getOrderNumber()))
+            if (s.equals(result.getOrderNumber())) {
                 s = generateOrderNumber();
-            else
+                logger.info("Generated a new order number {} due to duplication with result id: {}" + s + result.getId());
+            } else {
                 return s;
+            }
         }
         return s;
     }
