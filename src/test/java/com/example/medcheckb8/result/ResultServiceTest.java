886d96b2ec45a0adefa3c83cf473cd11d8b48b32
burulai <burulaiurbaeva7@gmail.com>
2023-06-07 11:19:48 +0600

    junit get result test

---
+++ src/test/java/com/example/medcheckb8/result/ResultServiceTest.java
@@ -0,0 +1,77 @@
+package com.example.medcheckb8.result;
+
+import com.example.medcheckb8.db.dto.request.ResultRequest;
+import com.example.medcheckb8.db.dto.response.ResultResponse;
+import com.example.medcheckb8.db.repository.ResultRepository;
+import com.example.medcheckb8.db.service.impl.ResultServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.webjars.NotFoundException;
+
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.stream;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResultServiceTest {
+
+    @Mock
+    private ResultRepository resultRepository;
+
+    @InjectMocks
+    private ResultServiceImpl resultService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void throwsExceptionIfOrderNumberInvalid(){
+        String orderNumber = "111111111115";
+
+        Mockito.when(resultService.getResult(orderNumber)).thenReturn(null);
+
+        assertThrows(NotFoundException.class, () ->
+            resultService.getResult(orderNumber),
+                "Result with order number: " + orderNumber + " doesn't exist.");
+    }
+
+
+    @Test
+    void testGetResult(){
+        String orderNumber = "11111111111";
+        ResultResponse resultResponse = ResultResponse.builder()
+                .patientId(2L)
+                .patientFullName("Кубан Келсинбеков")
+                .patientEmail("kubanmuit@gmail.com")
+                .patientPhoneNumber("+996708281456")
+                .dateOfIssue(LocalDate.of(2023, 6, 13))
+                .timeOfIssue(LocalTime.of(14,0,0))
+                .orderNumber("11111111111")
+                .name("ANESTHESIOLOGY")
+                .file("https://medcheckbucket.s3.eu-central-1.amazonaws.com/1685539128052hello.docx")
+                .build();
+
+
+        Mockito.when(resultRepository.getResultByOrderNumber(orderNumber)).thenReturn(Optional.of(resultResponse));
+        assertThat(resultResponse).isEqualTo(resultService.getResult(orderNumber));
+    }
+
+
+}
