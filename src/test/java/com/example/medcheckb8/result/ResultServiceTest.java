0491ce1f6f31361ae5dd3c9cea9f0f56e08a64a5
burulai <burulaiurbaeva7@gmail.com>
2023-06-07 13:57:48 +0600

    final review

--- src/test/java/com/example/medcheckb8/result/ResultServiceTest.java
+++ src/test/java/com/example/medcheckb8/result/ResultServiceTest.java
@@ -1,33 +1,19 @@
 package com.example.medcheckb8.result;
 
-import com.example.medcheckb8.db.dto.request.ResultRequest;
 import com.example.medcheckb8.db.dto.response.ResultResponse;
-import com.example.medcheckb8.db.entities.User;
 import com.example.medcheckb8.db.repository.ResultRepository;
 import com.example.medcheckb8.db.service.impl.ResultServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.webjars.NotFoundException;
-
 import java.time.LocalDate;
-
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.stream;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ResultServiceTest {
 
