e51ed27cd96cab1320cbe3cf18c28ae6cd229908
AYKI2 <abdulhamitov.bstn@gmail.com>
2023-06-14 10:52:26 +0600

    add method patientFindByID, add UserGetResultResponse and ResultUserResponse

--- src/main/java/com/example/medcheckb8/db/service/UserService.java
+++ src/main/java/com/example/medcheckb8/db/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.medcheckb8.db.service;
 import com.example.medcheckb8.db.dto.request.ProfileRequest;
 import com.example.medcheckb8.db.dto.response.ProfileResponse;
 import com.example.medcheckb8.db.dto.response.SimpleResponse;
+import com.example.medcheckb8.db.dto.response.UserGetResultResponse;
 import com.example.medcheckb8.db.dto.response.UserResponse;
 
 import java.util.List;
@@ -15,5 +16,6 @@ public interface UserService {
     ProfileResponse getResult();
 
     SimpleResponse deleteById(Long id);
+    UserGetResultResponse findById(Long id);
 
 }
