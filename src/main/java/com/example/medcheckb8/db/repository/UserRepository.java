b24970c1b9cab763c04b8fc5b7291815efedc178
AYKI2 <abdulhamitov.bstn@gmail.com>
2023-06-26 13:57:49 +0600

    refactor getAllPatients

--- src/main/java/com/example/medcheckb8/db/repository/UserRepository.java
+++ src/main/java/com/example/medcheckb8/db/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.medcheckb8.db.dto.response.UserResponse;
 import com.example.medcheckb8.db.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,12 +16,17 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByAccountId(Long id);
 
-    @Query("select distinct new com.example.medcheckb8.db.dto.response.UserResponse(u.id, u.firstName, u.lastName, u.phoneNumber, u.account.email, r.dateOfIssue,r.timeOfIssue) from User u join Result r where r.user.id = u.id and u.firstName <> 'Админ'")
+    @Query("SELECT DISTINCT new com.example.medcheckb8.db.dto.response.UserResponse(u.id, u.firstName, u.lastName, u.phoneNumber, u.account.email, r.dateOfIssue, r.timeOfIssue) " +
+            "FROM User u " +
+            "JOIN Result r ON r.user.id = u.id " +
+            "WHERE u.firstName <> 'Админ'")
     List<UserResponse> getAllPatients();
 
-    @Query("select distinct new com.example.medcheckb8.db.dto.response.UserResponse(u.id, u.firstName, u.lastName, u.phoneNumber, u.account.email, r.dateOfIssue,r.timeOfIssue) " +
-            "from User u  join Result r where r.user.id = u.id and u.firstName <> 'Админ' and" +
-            "(u.firstName like %:word% or u.lastName like %:word%  or u.account.email like %:word%) ")
-    List<UserResponse> getAllPatients(String word);
+    @Query("SELECT DISTINCT new com.example.medcheckb8.db.dto.response.UserResponse(u.id, u.firstName, u.lastName, u.phoneNumber, u.account.email, r.dateOfIssue, r.timeOfIssue) " +
+            "FROM User u " +
+            "JOIN Result r ON r.user.id = u.id " +
+            "WHERE u.firstName <> 'Админ' AND " +
+            "(u.firstName LIKE %:word% OR u.lastName LIKE %:word% OR u.account.email LIKE %:word%)")
+    List<UserResponse> getAllPatients(@Param("word") String word);
 
 }
