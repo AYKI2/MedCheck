5ad30ac012d1e07bf30410f11434b2bae439839c
AYKI2 <abdulhamitov.bstn@gmail.com>
2023-07-01 13:30:29 +0600

    refactor handlerMethodArgumentNotValidException

--- src/main/java/com/example/medcheckb8/db/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/medcheckb8/db/exceptions/handler/GlobalExceptionHandler.java
@@ -3,14 +3,10 @@ package com.example.medcheckb8.db.exceptions.handler;
 import com.example.medcheckb8.db.exceptions.*;
 import com.example.medcheckb8.db.dto.response.ExceptionResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -49,12 +45,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        BindingResult bindingResult = e.getBindingResult();
-        String error = bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining("; "));
         return new ExceptionResponse(
                 HttpStatus.BAD_REQUEST,
-                e.getClass().getSimpleName(),
-                error
+                e.getMessage(),
+                MethodArgumentNotValidException.class.getSimpleName()
         );
     }
     @ExceptionHandler(DownloadFailedException.class)
